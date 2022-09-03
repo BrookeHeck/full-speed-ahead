e4ee15f782766064792b23dd7ff1f5bb2d2469ff
Brooke Heck <b19heck@gmail.com>
2022-09-02 21:07:52 -0700

    bare bones react app

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Footer.css'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <h3>Brooke Heck</h3>
+    );
+  }
+}
+
+export default Footer;
